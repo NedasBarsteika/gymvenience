92413afa1869c97aa234fdf5de30f69c26ef2722
Kandy <rektelkid@gmail.com>
2025-03-17 23:47:38 +0200

    added buttons for accessing account information, order history and trainer visits history (not yet functional), and basic info about the page

--- src/pages/profilePage.tsx
+++ src/pages/profilePage.tsx
@@ -1,18 +1,46 @@
 // src/pages/profilePage.tsx
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Link, useNavigate } from 'react-router-dom';
 
 function ProfilePage() {
+    const navigate = useNavigate();
+
+
     return (
-      <div className="flex flex-col min-h-screen relative">
-        <Navbar />
-        
-        <div>Nigga
+        <div className="flex flex-col min-h-screen relative">
+            <Navbar />
+            <div className="flex-nowrap columns-2 min-h-screen relative">
+                <div className="absolute top-10 left-1/20 w-4/10">
+                    <div className="text-[50px] pb-3">Mano Paskyra</div>
+                    <div>Peržiurėkite savo užsakymus ir valdykite savo paskyros informaciją</div>
+                </div>
+                <div className="absolute flex flex-col top-10 right-1/20 w-4/10 gap-2">
+                    <Link
+                        to="/"
+                        className="border-r-4 font-semibold block py-2 px-3 text-gray-900 rounded hover:bg-gray-400 bg-gray-300 text-2xl"
+                    >
+                        Paskyros informacija
+                    </Link>
+                    <Link
+                        to="/"
+                        className="border-r-4 font-semibold block py-2 px-3 text-gray-900 rounded hover:bg-gray-400 bg-gray-300 text-2xl"
+                    >
+                        Užsakymų istorija
+                    </Link>
+                    <Link
+                        to="/"
+                        className="border-r-4 font-semibold block py-2 px-3 text-gray-900 rounded hover:bg-gray-400 bg-gray-300 text-2xl"  
+                                          >
+                        Vizitų istorija
+                    </Link>
+                </div>
+            </div>
+
+            <Footer />
         </div>
-        <Footer />
-      </div>
+
     );
-  }
-  
-  export default ProfilePage;
-  
+}
+
+export default ProfilePage;
