30b2e05ac6d3645bb20c9bb2b8929ee606294ce4
Kandy <rektelkid@gmail.com>
2025-04-30 17:13:26 +0300

    Weekday work calendar (semi functional)

--- src/components/SlotCard.tsx
+++ src/components/SlotCard.tsx
@@ -1,20 +1,46 @@
 // src/components/ui/SlotCard.tsx
-import React from "react";
+import React, { useState } from "react";
 
-const SlotCard = () => {
-    
-    return(
-        <div className=" flex-row align-bottom">
-            <div className="grid grid-cols-2 opacity-55">
-                <p className="align-middle">Vizito laikas</p>
-                <p className="align-middle">Trukmė</p>
-            </div>
-            <div className="grid grid-cols-2">
-                <input id="startTime" type="time" placeholder="3:00" className="p-2 w-35"></input>
-                <input id="duration" type="number" form="nn" placeholder="30" className="p-2 w-30"></input>
-            </div>
-        </div>
+function SlotCard({ weekDay }: any){
+
+    const [data, setData] = useState([{ time: new Date(Date.now()).toUTCString(), duration: "" }])
+
+    const handleClick = () => {
+        setData([...data, { time: new Date(Date.now()).toString(), duration: "0" }])
+    }
+    const handleChange = (e: any, i: any) => {
+        const {name, value} = e.target
+        console.log(weekDay)
+        const onchangeVal = [...data]
+        onchangeVal[i][name] = value
+        setData(onchangeVal)
+        localStorage.setItem("timetable" + weekDay, JSON.stringify(data));
+    }
+    const handleDelete = (i) => {
+        if(i>0){
+        const deleteVal = [...data]
+        deleteVal.splice(i, 1)
+        setData(deleteVal)
+        localStorage.setItem("timetable" + weekDay, JSON.stringify(data));
+        }
+    }
+
+    return (
+            data.map((values, i) =>
+                <div className="align-bottom">
+                    <div className="grid grid-rows-2 opacity-55">
+                        <p className="align-middle">Vizito laikas</p>
+                        <p className="align-middle">Trukmė</p>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <input onChange={(e) => handleChange(e, i)} name="date" value={values.time} id="startTime" type="time" placeholder="3:00" className="p-2 w-35"></input>
+                        <input onChange={(e) => handleChange(e, i)} name="duration" value={values.duration} id="duration" type="number" form="nn" placeholder="0" className="p-2 w-30"></input>
+                    </div>
+                    <button onClick={handleClick} className="border-2 rounded-lg w-10 text-[20px]">+</button>
+                    <button onClick={() => handleDelete(i)} className="border-2 rounded-lg w-10 text-[20px]">-</button>
+                </div>
+            )
     );
-};
+}
 
 export default SlotCard;
--- src/pages/schedulesEdit.tsx
+++ src/pages/schedulesEdit.tsx
@@ -5,40 +5,37 @@ import Footer from "../components/Footer";
 import { useState } from "react";
 import { motion } from 'framer-motion';
 import SlotCard from "../components/SlotCard";
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
 //import { Value } from "react-calendar/src/shared/types.js";
 
-//const weekDays = ["Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis", "Sekmadienis"];
+const weekDays = ["Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis", "Sekmadienis"];
 
 function SchedulesEdit() {
     var user = JSON.parse(localStorage.getItem("user") || '{}');
 
-    const [date, setDate] = useState<Value>(new Date());
+
     const [startTime, setStartTime] = useState("");
     const [duration, setDuration] = useState(0);
 
     async function handleSubmit(e: any) {
         e.preventDefault();
-        var durationUpdated = `${Math.floor(duration/60)}:${duration%60}`
-        var newDate = new Date(date.valueOf() + (3*3600*1000)).toISOString().slice(0, 10)
-        
+        var durationUpdated = `${Math.floor(duration / 60)}:${duration % 60}`
+
         //var formData = new FormData(e.target);
         //const formJson = Object.fromEntries(formData.entries());
 
-            await axios.post("https://localhost:7296/api/TrainerAvailability",
-                {
-                    trainerId: user.id,
-                    date: newDate,
-                    startTime: startTime,
-                    duration: durationUpdated,
-                    gymId: "01df7a45-db31-4065-9f11-ea4af52f06d4"
+        await axios.post("https://localhost:7296/api/TrainerAvailability",
+            {
+                trainerId: user.id,
+                date: newDate,
+                startTime: startTime,
+                duration: durationUpdated,
+                gymId: "01df7a45-db31-4065-9f11-ea4af52f06d4"
 
-                }
-            ).then(function (response: any) {
-                if (response != 200) {
-                }
-            });
+            }
+        ).then(function (response: any) {
+            if (response != 200) {
+            }
+        });
     }
 
     return (
@@ -61,27 +58,16 @@ function SchedulesEdit() {
                             <div className="text-[50px] pb-3">Darbo Laikas</div>
                             <div className="pb-6">Įterpkite vietą rezervacijai su jumis čia</div>
                             <div className="pb-3 ml-5 border-4">
-                                <div className="grid grid-cols-2">
-                                    <Calendar onChange={setDate} minDate={new Date(Date.now())} minDetail="month" value={date} />
-                                    <div>
-                                    <div className="grid grid-rows-2">
-                                        <p className="align-middle">Vizito laikas</p>
-                                        <input id="startTime" type="time" placeholder="3:00" className="p-2 border-1 border-gray-600 rounded" onChange={(e:any) => setStartTime(e.target.value)} value={startTime}></input>
-                                        
-                                    </div>
-                                    <div className="grid grid-rows-2">
-                                        <p className="text-center align-text-bottom">Trukmė (Minutėmis)</p>
-                                        <input id="duration" type="number" placeholder="30" className="p-2 border-1 border-gray-600 rounded" onChange={(e:any) => setDuration(e.target.value)} value={duration}></input>
-                                    </div>
-                                    </div>
-                                    {/*weekDays.map((day) =>
-                                        <div className="flex align-center h-1/10">
+                                <div className="grid grid-cols-7 p-5">
+                                {weekDays.map((day) =>
+                                        <div className="flex-rowalign-center h-50">
                                             <div>
-                                                <p className="p-5 text-[24px]">{day}</p>
-                                                <button onClick={appendSlot} className="border-2 rounded-lg w-20 text-[20px]">+</button>
+                                                <p className="flex p-5 text-[24px]">{day}</p>
+                                                <SlotCard 
+                                                weekDay = {day} 
+                                                />
                                             </div>
-                                            <SlotCard />
-                                        </div>)*/}
+                                        </div>)}
                                 </div>
                             </div>
                             <button className="rounded-lg radius-4 ml-5 mt-3 hover:cursor-pointer w-30 h-10 hover:bg-gray-400 bg-gray-300" onClick={handleSubmit}>Išsaugoti</button>
